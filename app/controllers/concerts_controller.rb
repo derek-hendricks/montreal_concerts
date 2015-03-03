25b2e2b58d37b91ae8e3e3c9e13988827b2d88a0
Derek Hendricks <derek.m.hendricks@gmail.com>
2015-03-03 16:14:11 -0500

    display tracks list of an artist from index local listings page

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,11 +1,21 @@
 class ConcertsController < ApplicationController
-	
+	include ConcertsHelper
+	before_filter :get_params
+	before_filter :soundcloud_connect, :only => :show
+
 	def index
 		@doc = Nokogiri::HTML(open('http://www.cheapthrills.ca/news.html'), nil, 'ISO-8859-1')
 		@doc.encoding = 'UTF-8'
 	end
 
 	def show
+		@aristMusic = @client.get('/tracks', :q => @artist)
 	end
+
+	private 
+
+	  def get_params
+	  	@artist = params[:id]
+	  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def soundcloud_connect
+		@client ||= Soundcloud.new(:client_id => ENV["CLIENT_ID"],
+	                        :client_secret => ENV["CLIENT_SECRET"],
+	                        :redirect_uri => ENV["REDIRECT_URL"])
+	end
 end
