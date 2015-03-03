a2e3747558104976a116e3238c76a69dc511e779
Derek Hendricks <derek.m.hendricks@gmail.com>
2015-03-03 15:43:41 -0500

    display concert listings from cheapthrills.ca news page

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,6 +1,11 @@
 class ConcertsController < ApplicationController
+	
 	def index
+		@doc = Nokogiri::HTML(open('http://www.cheapthrills.ca/news.html'), nil, 'ISO-8859-1')
+		@doc.encoding = 'UTF-8'
 	end
+
 	def show
 	end
+
 end
