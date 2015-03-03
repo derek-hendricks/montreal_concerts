270bdbca0052d92de82ac5249e440c919bec1aa1
derek-hendricks <derek.m.hendricks@gmail.com>
2015-03-03 18:01:23 -0500

    Update concerts_helper.rb

--- app/helpers/concerts_helper.rb
+++ app/helpers/concerts_helper.rb
@@ -1,36 +1,36 @@
 module ConcertsHelper
 	
-	def sign_in
-		code = params[:code]
-		if (code && !session[:user])
-			@client = Soundcloud.new(:client_id => ENV["CLIENT_ID"],
-	                        :client_secret => ENV["CLIENT_SECRET"],
-	                        :redirect_uri => ENV["REDIRECT_URL"])
-			session[:token] = @client.exchange_token(:code => code)
-			access_token = @client.exchange_token(:code => code)
-			current_user = @client.get('/me')
-			session[:user] = current_user.username
-		end
-	end
+  def sign_in
+    code = params[:code]
+      if (code && !session[:user])
+        @client = Soundcloud.new(:client_id => ENV["CLIENT_ID"],
+	                    :client_secret => ENV["CLIENT_SECRET"],
+	                    :redirect_uri => ENV["REDIRECT_URL"])
+	session[:token] = @client.exchange_token(:code => code)
+	access_token = @client.exchange_token(:code => code)
+	current_user = @client.get('/me')
+        session[:user] = current_user.username
+      end
+    end
 
-	def get_track_comments
-  	@track = @client.get('/tracks/' + (@track_id || @like_track_id))
-		@comments = Array.new
-		@client.get("/tracks/#{@track_id || @like_track_id}/comments").each do |comment|
-			@comments.push(comment)
-		end
-	end
+  def get_track_comments
+    @track = @client.get('/tracks/' + (@track_id || @like_track_id))
+    @comments = Array.new
+    @client.get("/tracks/#{@track_id || @like_track_id}/comments").each do |comment|
+      @comments.push(comment)
+    end
+  end
 
-	def like track_id
-  	@client = Soundcloud.new(:access_token => session[:token]['access_token'])
-		@track = @client.get('/tracks/'+track_id)
-		@client.put("/me/favorites/#{@track.id}")
-	end
+  def like track_id
+    @client = Soundcloud.new(:access_token => session[:token]['access_token'])
+    @track = @client.get('/tracks/'+track_id)
+    @client.put("/me/favorites/#{@track.id}")
+  end
 
-	def embed_soundcloud_widget track_id
+  def embed_soundcloud_widget track_id
     track = @client.get('/tracks/' + track_id)
-	 	@embed_info = @client.get('/oembed', :url => track.permalink_url)
-	 	@embedded_track = @embed_info['html'].html_safe
-	end
+    @embed_info = @client.get('/oembed', :url => track.permalink_url)
+    @embedded_track = @embed_info['html'].html_safe
+  end
 
 end
