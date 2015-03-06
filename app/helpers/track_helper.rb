cdc7acb858544f644e0bd93b967431e254cde0b5
Derek Hendricks <derek.m.hendricks@gmail.com>
2015-03-06 16:44:36 -0500

    indent

--- app/helpers/track_helper.rb
+++ app/helpers/track_helper.rb
@@ -1,14 +1,11 @@
 module TrackHelper 
 
-	def find_url string
-		url_array = [] 
-		string.scan(/(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/).each do |url| 
-			url_array.push(url[0])
-		end
-		return url_array
-	end
+  def find_url string
+    url_array = [] 
+    string.scan(/(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/).each do |url| 
+      url_array.push(url[0])
+    end
+    return url_array
+  end
 
 end
-
-
-# https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/
